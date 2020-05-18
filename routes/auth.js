b363422ef44a904d765a6aecad8bde8fe9ccba49
a233057 <meluleki.madlala@standardbank.co.za>
2020-05-18 14:20:25 +0200

    check if user exists, and hashing the password with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation } = require('../validation');
 
 router.post('/register', async (req,res) => {
@@ -11,10 +12,22 @@ router.post('/register', async (req,res) => {
         return res.status(400).send(error.details[0].message);
     }
     
+    //Check if user exists in the db
+    const emailExist = await User.findOne({email: req.body.email});
+
+    if (emailExist) {
+        return res.status(400).send('Email already exists')
+    }
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10); 
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
