9350b75be791d1d73142a98e826ca8ffffe6bd54
a233057 <meluleki.madlala@standardbank.co.za>
2020-05-18 14:04:03 +0200

    Added @hapi/Joi validation for requests

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation } = require('../validation');
 
 router.post('/register', async (req,res) => {
+
+    //Validate data before we save
+     const { error } = registerValidation(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    
     const user = new User({
         name: req.body.name,
         email: req.body.email,
